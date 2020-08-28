7234a8b4482b6b4762f8abea1497373b1ddcadbe
caroline <carolinejepps@gmail.com>
2020-08-28 16:27:27 +0000

    Added db files and schema

---
+++ db/seeds/parks.sql
@@ -0,0 +1,17 @@
+INSERT INTO parks (name, image, description)
+VALUES ('Cypress','https://lh3.googleusercontent.com/proxy/4IYcjfJoSXUE5e6d82I5euxlgku9v7r9vdnhyMjtVcoTg8vpfIoodcXlVJRyOYa641L4axcZ2114w3gsrGfvhoKDak_AE6lfoqtusIYdnPLVOaj_Sm8', 'Upper mountain trails including the Howe Sound Crest Trail, Hollyburn Mountain trails, and the Black Mountain Plateau trails.');
+
+INSERT INTO parks (name, image, description)
+VALUES ('Garibaldi','https://lh3.googleusercontent.com/proxy/4IYcjfJoSXUE5e6d82I5euxlgku9v7r9vdnhyMjtVcoTg8vpfIoodcXlVJRyOYa641L4axcZ2114w3gsrGfvhoKDak_AE6lfoqtusIYdnPLVOaj_Sm8', 'Diamond Head (Elfin Lakes), Rubble Creek (Garibaldi Lake) and Cheakamus trailheads');
+
+INSERT INTO parks (name, image, description)
+VALUES ('Golden Ears','https://lh3.googleusercontent.com/proxy/4IYcjfJoSXUE5e6d82I5euxlgku9v7r9vdnhyMjtVcoTg8vpfIoodcXlVJRyOYa641L4axcZ2114w3gsrGfvhoKDak_AE6lfoqtusIYdnPLVOaj_Sm8', 'South Beach Day-Use Parking Lot, Alouette Lake Boat Launch Parking, East and West Canyon Trailhead Parking Lot (including Lower Falls Trail).');
+
+INSERT INTO parks (name, image, description)
+VALUES ('Mount Robson','https://lh3.googleusercontent.com/proxy/4IYcjfJoSXUE5e6d82I5euxlgku9v7r9vdnhyMjtVcoTg8vpfIoodcXlVJRyOYa641L4axcZ2114w3gsrGfvhoKDak_AE6lfoqtusIYdnPLVOaj_Sm8', 'Berg Lake Trail');
+
+INSERT INTO parks (name, image, description)
+VALUES ('Mount Seymour','https://lh3.googleusercontent.com/proxy/4IYcjfJoSXUE5e6d82I5euxlgku9v7r9vdnhyMjtVcoTg8vpfIoodcXlVJRyOYa641L4axcZ2114w3gsrGfvhoKDak_AE6lfoqtusIYdnPLVOaj_Sm8', 'Upper mountain trails including the Seymour Main Trail, Mystery Lake Trail, and Dog Mountain Trail.');
+
+INSERT INTO parks (name, image, description)
+VALUES ('Stawamus Chief','https://lh3.googleusercontent.com/proxy/4IYcjfJoSXUE5e6d82I5euxlgku9v7r9vdnhyMjtVcoTg8vpfIoodcXlVJRyOYa641L4axcZ2114w3gsrGfvhoKDak_AE6lfoqtusIYdnPLVOaj_Sm8', 'Chief Peaks Trail (also called the Backside Trail)');

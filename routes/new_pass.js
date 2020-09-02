c94dd7fef4629c9a4f7aee1390347d08b57404ef
Hanna Oh Andrews <hannaohandrews@Dan-PC.edptest.internal>
2020-09-02 10:40:18 -0700

    adding put requests

--- routes/new_pass.js
+++ routes/new_pass.js
@@ -34,22 +34,35 @@ module.exports = db => {
     }
 
     // const { student, interviewer } = req.body.interview;
-
+    console.log(req.body)
     db.query(
       `
-      INSERT INTO guests (first_name, last_name, phone,entry_id)
+      INSERT INTO guests (first_name, last_name, phone, entry_id)
       VALUES($1::text, $2::text, $3::integer,$4::integer)
     `,
-    [first_name, last_name, phone, Number(request.params.id)]
+    [req.body.first_name,req.body.last_name, req.body.phone, Number(req.params.id)])
+
     .then(result => {
-        res.status(200).json({});
-        updateGuest();
-      })
+      return db.query(
+      `
+      SELECT *
+      FROM pass_entries
+      JOIN guests ON guests.entry_id = pass_entries.id
+      JOIN visitors ON visitor_id = visitors.id
+      JOIN trails ON trail_id = trails.id
+    `)
+    })
+
+    .then(result => {
+      res.status(200).json({pass_entries: result.rows});
+    })
+
     .catch(err => {
       res
         .status(500)
         .json({ error: err.message });
     });
+
   });
 
   

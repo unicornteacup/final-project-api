f106f377c37e2393346b24e654ab99ad08ad8f0b
Hanna Oh Andrews <hannaohandrews@Dan-PC.edptest.internal>
2020-09-07 11:19:58 -0700

    visitors

--- routes/visitors.js
+++ routes/visitors.js
@@ -18,5 +18,25 @@ module.exports = db => {
     });
   });
 
+
+  // INSERTING visitors into visitors
+  router.post("/visitors", (req,res) => {
+    db.query(
+        `
+        INSERT INTO visitors (first_name,last_name, phone, email_address, password)
+        VALUES($1::text, $2::text, $3::integer, $4::integer)
+        RETURNING *
+        `,[req.body.first_name, req.body.last_name, req.body.phone, req.query.entry_id])
+
+    .then(result => {
+      res.status(200).json({guests: result.rows});
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  });
+
   return router;
 };

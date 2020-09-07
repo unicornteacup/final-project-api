94d3f7655fdd54dc7a198aeb46631b4f0ecdc070
sE7rgo <se7rgo@gmail.com>
2020-09-07 10:42:58 -0700

    create new visitor route

--- routes/visitors.js
+++ routes/visitors.js
@@ -18,5 +18,24 @@ module.exports = db => {
     });
   });
 
+
+  router.post("/visitors", (req,res) => {
+    db.query(
+        `
+        UPDATE visitors
+        SET visitors_first_name =$1::text, visitors_last_name =$2::text, visitors_phone =$3::integer, visitors_email =$4::text, visitors_password =$5::text
+        RETURNING *
+        `,[req.body.first_name, req.body.last_name, req.body.phone, req.body.email, req.body.password])
+ 
+    .then(result => {
+      res.status(200).json({visitors: result.rows});
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .json({ error: err.message });
+    });
+  });
+
   return router;
 };

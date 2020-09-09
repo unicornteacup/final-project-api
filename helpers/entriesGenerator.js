5f0d199364d03e42b59649b6ccc040549d7f428d
sE7rgo <se7rgo@gmail.com>
2020-09-09 16:16:52 -0700

    resolving conflict

--- helpers/entriesGenerator.js
+++ helpers/entriesGenerator.js
@@ -1,49 +1,54 @@
-// const fs = require('fs');
+const fs = require('fs');
 
-// userSeedsGenerator = (times) => {
-//   let array = [];
-//   let num = 50;
+userSeedsGenerator = (times) => {
+  let array = [];
+  let num = 50;
 
-//   while (num < times) {
-//     let entry = `
-//       INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
-//       VALUES ('Sep 10 2020', 'Pending', '1','${num}');
-//       INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
-//       VALUES ('Sep 12 2020', 'Pending', '1,'${num}');
-//       INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
-//       VALUES ('Sep 14 2020', 'Pending', '1','${num}');
-//       INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
-//       VALUES ('Sep 10 2020', 'Pending', '4','${num}');
-//       INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
-//       VALUES ('Sep 12 2020', 'Pending', '4','${num}');
-//       INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
-//       VALUES ('Sep 14 2020', 'Pending', '4','${num}');
-//       INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
-//       VALUES ('Sep 10 2020', 'Pending', '7','${num}');
-//       INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
-//       VALUES ('Sep 12 2020', 'Pending', '7','${num}');
-//       INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
-//       VALUES ('Sep 14 2020', 'Pending', '7','${num}');
-//       INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
-//       VALUES ('Sep 10 2020', 'Pending', '10','${num}');
-//       INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
-//       VALUES ('Sep 12 2020', 'Pending', '10','${num}');
-//       INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
-//       VALUES ('Sep 14 2020', 'Pending', '10','${num}');`
-//     array += entry;
-//     num++;
-//   }
-//   return array;
-// }
+  while (num < times) {
+    let entry = `
+      INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
+      VALUES ('Sep 10 2020', 'Pending', '1','${num}');
+      INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
+      VALUES ('Sep 12 2020', 'Pending', '1,'${num}');
+      INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
+      VALUES ('Sep 14 2020', 'Pending', '1','${num}');
+      INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
+      VALUES ('Sep 10 2020', 'Pending', '4','${num}');
+      INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
+      VALUES ('Sep 12 2020', 'Pending', '4','${num}');
+      INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
+      VALUES ('Sep 14 2020', 'Pending', '4','${num}');
+      INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
+      VALUES ('Sep 10 2020', 'Pending', '7','${num}');
+      INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
+      VALUES ('Sep 12 2020', 'Pending', '7','${num}');
+      INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
+      VALUES ('Sep 14 2020', 'Pending', '7','${num}');
+      INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
+      VALUES ('Sep 10 2020', 'Pending', '10','${num}');
+      INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
+      VALUES ('Sep 12 2020', 'Pending', '10','${num}');
+      INSERT INTO pass_entries (date, status, trail_id, visitor_id) 
+      VALUES ('Sep 14 2020', 'Pending', '10','${num}');`
+    array += entry;
+    num++;
+  }
+  return array;
+}
 
-// fs.writeFile('../db/seeds/entries/pass_entries_6.sql', userSeedsGenerator(120), (err) => {
-//   if (err) throw err;
-//   console.log('The file has been saved!');
-// });
+fs.writeFile('../db/seeds/entries/pass_entries_6.sql', userSeedsGenerator(120), (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
 
-let date = new Date;
-  date.setHours(0, 0, 0, 0)
-  date = date.setDate(date.getDate() + 1);
-  date = new Date(date)
-  date = date.toDateString().slice(4)
-  console.log(date)
+const QRCode = require('qrcode')
+ 
+QRCode.toFile('./qr.png', 'Some text', {
+  color: {
+    dark: '#00F',  // Blue dots
+    light: '#0000' // Transparent background
+  }
+}, function (err) {
+  if (err) throw err
+  console.log('done')
+})
--- helpers/getWinners.js
+++ helpers/getWinners.js
@@ -1,29 +1,45 @@
-const winners = (entries, resultEntries) => {
+const { updateEntry } = require('../services/updateEntries');
+
+const processWinners = (db, entries, resultEntries) => new Promise((resolve, reject) => {
+  // console.log('winner', entries, resultEntries)
   //lottery is true if we have more users then resultUsers
-  if (entries < resultEntries) return entries;
-
-  let count = 0;
-  //defining oldArray, because of splice, it will mutate initial state
-  let oldArray = entries;
-  let newArray = [];
-  while (count < resultEntries) {
-    //lottery itself :)
-    const randomIndex = Math.floor(Math.random() * Math.floor(resultEntries));
-    // because of mutation we can get undefined
-    if (oldArray[randomIndex] !== undefined) {
-      // updateWinners(oldArray[randomIndex])
-
-      newArray.push(oldArray[randomIndex]);
-
-
-      oldArray.splice(randomIndex, 1)
-      count++;
-    }
+  let updateEntries = [];
+  if (entries.length < resultEntries) {
+    entries.map((entry) => {
+      updateEntries.push(updateEntry(db, 'Success', entry.id))
+    })
+  } else {
+      let count = 0;
+      //defining oldArray, because of splice, it will mutate initial state
+      let oldArray = entries;
+      let newArray = [];
+      while (count < resultEntries) {
+        //lottery itself :)
+        const randomIndex = Math.floor(Math.random() * Math.floor(resultEntries));
+        // because of mutation we can get undefined
+        if (oldArray[randomIndex] !== undefined) {
+    
+          updateEntries.push(updateEntry(db, 'Success', oldArray[randomIndex].id))
+    
+          newArray.push(oldArray[randomIndex]);
+    
+    
+          oldArray.splice(randomIndex, 1)
+          count++;
+        }
+      }
+      oldArray.map((entry) => {
+        updateEntries.push(updateEntry(db, 'Declined', entry.id))
+      })
   }
-  // oldArray.map((entry) => {
-  //   // updateLosers(entry);
-  // })
-  return newArray;
-}
+  
+  Promise.all(updateEntries)
+  .then(() => {
+    resolve()
+  })
+  .catch(() => {
+    reject()
+  })
+})
 
-module.exports = { winners };
+module.exports = { processWinners };
